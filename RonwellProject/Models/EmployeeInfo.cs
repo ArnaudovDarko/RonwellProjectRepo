2c38ece2f7bdbbbc435102b9f39c551d6656873f
Darko Arnaudov <darko@LAPTOP-58OCJBD4>
2024-01-13 11:24:56 +0100

    Created Data Model called "EmployeeInfo"

---
+++ RonwellProject/Models/EmployeeInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RonwellProject.Models
+{
+    public class EmployeeInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Salary { get; set; }
+
+    }
+}
